842ce30533a33e82f10ad9b0a18198ea6b2f9388
ShaikSony18 <161578362+ShaikSony18@users.noreply.github.com>
2024-09-24 17:58:45 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,63 @@
+// //import {useState } from "react";
+// //import './App.css';
+// //const REACTION = ["🥳","🥳","😆"];
+// //function App() {
+//   //let count = 100;
+//   //syntax : const[var, setVar] = useState(initial_value); 
+//   //const [count,setCount] = useState(0);
+//   //const [emoji,setEmoji] = useState(REACTION[0]);
+//   //function handleClick() {
+//     //if(count < REACTION.length - 1)
+//       //setEmoji(REACTION[count]);
+//   //else {
+//     //count++
+//     //setEmoji(REACTION[REACTION.length - 1]);
+//   //}
+//   //setCount(count + 1)
+//   //return  ( 
+//    // <div>
+//      // <button  id = "btn" onClick = {handleClick} > {count} {emoji} </button>
+//     //</div>
+//   //);
+// //}
+// //}
+// //export default App;
+// import Activity from "./component/Activity";
+// function App() {
+//   return (
+//     <div>
+//       <Activity /> 
+//     </div>
+//   );
+// }
+// export default App;
+
+//import { formatPostcssSourceMap } from "vite"
+
+
+
+//App.jsx(calculator)
+import ToDo from "./component/ToDoList/ToDo";
+
+function App() {
+  return (
+    <>
+      <ToDo />
+    </>
+  );
+}
+export default App;
+
+
+
+
+//</div>chess
+// import Chess from"./component/Chess/Chess";
+// function App() {
+//   return(
+//     <div>
+//       <Chess/>
+//     </div>
+//   );
+// }export default App()
+
